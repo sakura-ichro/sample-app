c6ba14e4a4f22b080f2427edc9444374a5e58299
sakura-ichro <sakura.ichro@gmail.com>
2019-07-26 10:38:51 +0900

    deploy commit

---
+++ src/components/form/date-picker.vue
@@ -0,0 +1,35 @@
+<template lang='pug'>
+div
+  .v-calendar
+    .v-calendar-input
+      slot
+
+    .v-calendar-body
+      Datepicker(
+        input-class="input"
+        :language="locale"
+        format="yyyy年M月d日"
+        :clear-button="true"
+      )
+
+</template>
+<script>
+import Datepicker from 'vuejs-datepicker'
+import { ja } from 'vuejs-datepicker/dist/locale'
+export default {
+  components: {
+    Datepicker
+  },
+  data() {
+    return {
+      locale: ja
+    }
+  }
+}
+</script>
+<style lang='sass' scoped>
+.v-calendar
+
+  &-input
+
+</style>
---
+++ src/pages/datepicker/index.vue
@@ -0,0 +1,14 @@
+<template lang='pug'>
+div
+  Datepicker
+
+</template>
+<script>
+import Datepicker from '@c/form/date-picker'
+export default {
+  components: {
+    Datepicker
+  },
+}
+</script>
+<style lang='sass' scoped></style>
